4115254f43f78759a61741ccea3aaddc003afada
alex <alexmagikov228@gmail.com>
2024-09-17 20:31:08 +0300

    18.09/major

---
+++ homework/functions.h
@@ -0,0 +1,157 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void swap(int* num1, int* num2) {
+	*num1 = *num2 + *num1;
+	*num2 = *num1 - *num2;
+	*num1 = *num1 - *num2;
+}
+
+int searchNumberOfZeroElements(int array[], int numberOfElements) {
+	int numberOfZeroElements = 0;
+	for (int i = 0; i < numberOfElements; i++) {
+		if (array[i] == 0) {
+			numberOfZeroElements++;
+		};
+	};
+	return numberOfZeroElements;
+}
+
+int* createArray(int numberOfElements) {
+	int* array = (int*)malloc(numberOfElements * sizeof(int)); //выделение памяти для массива длиной numberOfElements
+	return array;
+}
+
+bool isBalanced(char string[]) {
+	int numOfLeftBracket = 0;
+	int numOfRightBracket = 0;
+	for (int i = 0; i < 20; i++) {
+		if (string[i] != '\0') {
+			if (string[i] == '(') {
+				numOfLeftBracket++;
+			}
+			else if (string[i] == ')') {
+				numOfRightBracket++;
+			}
+		}
+		else {
+			break;
+		}
+	}
+	if (numOfLeftBracket == numOfRightBracket) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+bool isSimple(int num) {
+	bool isTrue = true;
+	for (int i = 2; i <= num / 2; i++) {
+		if (num % i == 0) {
+			isTrue = false;
+		}
+	}
+	if (isTrue) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+int happyTickets(void) {
+	int arrayOfPossibleAmounts[28] = {};
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			for (int k = 0; k < 10; k++) {
+				arrayOfPossibleAmounts[(i + j + k)]++;
+			}
+		}
+	}
+	int numOfHappyTickets = 0;
+	for (int i = 0; i < 28; i++) {
+		numOfHappyTickets = numOfHappyTickets + arrayOfPossibleAmounts[i] * arrayOfPossibleAmounts[i];
+	}
+	return numOfHappyTickets;
+}
+
+int searchLengthString(char string[]) {
+	int lengthString = 0;
+	for (int i = 0; i < 20; i++) {
+		if (string[i] != '\0') {
+			lengthString++;
+		}
+		else {
+			break;
+		}
+	}
+	return lengthString - 1;
+}
+
+int searchNumOccurence(char string1[], char string2[]) {
+	int numOfOccurence = 0;
+	int lengthString1 = searchLengthString(string1);
+	int lengthString2 = searchLengthString(string2);
+	for (int i = 0; i < lengthString1; i++) {
+		if (string1[i] != '\0') {
+			bool isOccurence = true;
+			for (int j = 0; j < lengthString2; j++) {
+				if (string1[i + j] != string2[j]) {
+					isOccurence = false;
+				}
+			}
+			if (isOccurence) {
+				numOfOccurence++; 
+			}
+		}
+		else {
+			break;
+		}
+	}
+	return numOfOccurence;
+}
+
+void swapPartOfArray(int array[], int startIndex, int finshIndex) {
+	if ((finshIndex - startIndex + 1) % 2 == 0) {
+		int numPairs = 0;
+		while (numPairs != (finshIndex - startIndex + 1) / 2 ) {
+			swap(&array[startIndex + numPairs], &array[finshIndex - numPairs]);
+			numPairs++;
+		}
+	}
+	else {
+		int numPairs = 0;
+		while (numPairs != (finshIndex - startIndex) / 2) {
+			swap(&array[startIndex + numPairs], &array[finshIndex - numPairs]);
+			numPairs++;
+		}
+	}
+}
+
+int searchQuotient(int divisible, int divider) {
+	if (divisible == 0) {
+		return 0;
+	}
+	else if (divisible > 0) {
+		int quotient = 0;
+		while (divisible >= divider) {
+			quotient++;
+			divisible = divisible - abs(divider);
+		}
+		return quotient;
+	}
+	else {
+		int quotient = 1;
+		while (divider * quotient < abs(divisible)) {
+			quotient++;
+		}
+		return -quotient;
+	}
+}
+
+
+
---
+++ homework/homework(18.09).c
@@ -0,0 +1,98 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "functions.h"
+
+void firstTask(void) {
+	int numberOfElements;
+	printf("Input int num of elements of array ");
+	scanf("%d", &numberOfElements);
+	int* array = createArray(numberOfElements); 
+	for (int i = 0; i < numberOfElements; i++) {
+		printf("Input int element ");
+		scanf("%d", &array[i]);
+	}
+
+	int numberOfZeroElements;
+	numberOfZeroElements = searchNumberOfZeroElements(array, numberOfElements);
+	printf("Number of zero elements: %d", numberOfZeroElements);
+
+	free(array);
+}
+
+void secondTask(void) {
+	int a = 1;
+	int b = 2; 
+	swap(&a, &b);
+	printf("%d %d", a, b);
+}
+
+void thirdTask(void) {
+	int divisible = 0;
+	int divider = 0; 
+	printf("Input int divisible ");
+	scanf("%d", &divisible);
+	printf("Input int divider ");
+	scanf("%d", &divider);
+	printf("%d", searchQuotient(divisible, divider));
+}
+
+void fourthTask(void) {
+	printf("%d", happyTickets());
+}
+
+void fifthTask(void) {
+	char string[20] = "";
+	printf("Input string with max of 20 letterss ");
+	fgets(string, 20, stdin);
+	if (isBalanced(string)) {
+		printf("balanced");
+	}
+	else {
+		printf("unbalanced");
+	}
+}
+
+void sixthTask(void) {
+	int num;
+	int checkNum;
+	printf("Input unsigned int num ");
+	scanf("%d", &num);
+	for (checkNum = 2; checkNum < num; checkNum++) {
+		if (isSimple(checkNum)) {
+			printf("%d ", checkNum);
+		}
+	}
+}
+
+void seventhTask(void) {
+	char string1[20] = "";
+	char string2[20] = "";
+	printf("Input first string with  max 20 letters ");
+	fgets(string1, 20, stdin);
+	printf("Input shorter string with  max 20 letters ");
+	fgets(string2, 20, stdin);
+	printf("%d", searchNumOccurence(string1, string2));
+}
+
+void eighthTask(void) {
+	int numberOfElements;
+	printf("Input int length of array ");
+	scanf("%d", &numberOfElements);
+	int* array = createArray(numberOfElements);
+	for (int i = 0; i < numberOfElements; i++) {
+		printf("Input int element ");
+		scanf("%d", &array[i]);
+	}
+	int lengthPart1;
+	printf("Input int length of part1  ");
+	scanf("%d", &lengthPart1);
+	swapPartOfArray(array, 0, lengthPart1 - 1);
+	swapPartOfArray(array, lengthPart1, numberOfElements - 1);
+	swapPartOfArray(array, 0, numberOfElements - 1);
+	for (int i = 0; i < numberOfElements; i++) {
+		printf("%d ", array[i]);
+	}
+}	
+
+void main(void) {
+	thirdTask();
+}
