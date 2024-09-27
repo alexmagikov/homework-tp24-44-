ed6f2864088abbfd03da6711ab3bf44c809d5600
alex <alexmagikov228@gmail.com>
2024-09-27 22:14:12 +0300

    new directory

---
+++ semiQsort/semiQsort/tests.c
@@ -0,0 +1,10 @@
+#include "stdlib.h"
+#include "time.h"
+#include "stdio.h"
+#include "stdbool.h"
+#include "main.c"
+
+bool isSorted(void) {
+	int numElements = 10;
+	int* array = createArray(numElements);
+}
