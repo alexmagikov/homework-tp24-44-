85448c30002d694fcd7064fc6a72a7203f0d6183
alex <alexmagikov228@gmail.com>
2024-10-09 15:14:04 +0300

    3 task

--- control work/printToFile/printToFile/main.c
+++ control work/printToFile/printToFile/main.c
@@ -9,17 +9,18 @@ void main(void) {
 	fileToPrint = fopen("fileToPrint.txt", "w");
 	char string[10] = "";
 	char stringToPrintAll[30] = "";
+	int startIndex = 0;
+	int endIndex = 10;
 	while (fgets(string, 10, fileToRead) != NULL) {
 		printf("%s", string);
 		char* stringToPrint;
 		stringToPrint = string;
-		for (int i = 0; i < 10; i++) {
+		for (int i = startIndex; i < endIndex; i++) {
 			stringToPrintAll[i] = stringToPrint[i];
 		}
-		printf("%s", stringToPrintAll);
 	}
-	
-	while()
+	printf("%s", stringToPrintAll);
+	//while()
 
 	fclose(fileToRead);
 	fclose(fileToPrint);
